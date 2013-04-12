197738b17363219b8b50efedccd81271fc055ef3
Walmir Neto <wfsneto@gmail.com>
2013-04-11 22:22:43 -0300

    create new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTasks::Application.config.secret_token = '5fe1e58b89621f438c2652765d6043ff00f1712e8b7792aa4eb723c57b8999d29fe6fbe19a73fdba663e1a46fb79f16f9148d59445dc781675d115d11daa6909'
